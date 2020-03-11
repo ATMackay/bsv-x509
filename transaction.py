e5de6099fbefc32d90c17130e0d15196967272db
Alexander Mackay <a.mackay@nchain.com>
2020-03-11 13:52:14 +0000

    Update OP_RETURN extraction.

--- transaction.py
+++ transaction.py
@@ -18,7 +18,10 @@ def retrieve_tx(txid):
     request_json = json.loads(request_url.text)
     return request_json
 
-def extract_nulldata(txid, v_out):
+def extract_certificate(txid, v_out):
+    """
+    Extracts OP_RETURN data and creates Python list/dict containing field values that can be accessed.
+    """
     v_out = int(v_out)
     if type(txid) != str or len(txid) > 64:
         raise Exception("input txid not formatted correctly!. txid must be a string of length 64.")
@@ -29,30 +32,24 @@ def extract_nulldata(txid, v_out):
     if sys.getsizeof(target_data_list) > tx_out_size_limit:
         raise Exception("Tx too large to parse.")
 
-    target_opreturn = target_data_list.get('scriptPubKey').get('hex')
+    target_opreturn = target_data_list.get('scriptPubKey').get('opReturn').get('parts')
     # Check that output is a valid OP_RETURN or OP_FALSE OP_RETURN
     if target_opreturn[0:4] == '006a':
-        return target_opreturn[4:]
+        return json.loads(target_opreturn[1])
     elif target_opreturn[0:2] == '6a':
-        return target_opreturn[2:]
+        return json.loads(target_opreturn[1])
     else:
-        raise Exception("Not a valid data outpoint")
+        raise Exception("Not a valid data outpoint.")
 
 def check_opreturn_prefix(opreturn):
     # Check if OP_RETURN  uses the CA prefix
     # will pass if prefix is used or not, however the certificate viewer may return an error 
-    if opreturn[2:10] == str(x509_builder.ca_prefix) or opreturn[4:12] == str(x509_builder.ca_prefix) :
+    if opreturn[0] == str(x509_builder.ca_prefix) or opreturn[0] == str(x509_builder.ca_prefix) :
         return True
     else:
         print("Warning: Incorrect protocol identifier. Certificate may not be viewable.")
         return True
 
-def get_certificate(opret):
-    """
-    This function takes the opreturn bytes from a transaction and 
-    returns json formatted certificate.
-    """
-    return True
    
 
 def get_pubkeys(txid):
@@ -62,6 +59,7 @@ def get_pubkeys(txid):
     key_list = dict()
     for i in range(num_inputs):
         scriptsig = target_tx_inputs[i].get('scriptSig').get('asm')
+        # Must be P2PKH transaction
         key_list[i]= str(scriptsig)[len(scriptsig)-66:]
         if key_list.get(i)[0:2] != '02' and key_list.get(i)[0:2] != '03':
             raise Exception("Error: Inputs must be P2PKH.")
